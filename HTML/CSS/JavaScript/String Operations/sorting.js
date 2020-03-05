109dcd058cd79f405856bc1c14bb2313a44a21a4
undefinedzack <adhney.nawghare@gmail.com>
2020-03-05 10:48:32 +0530

    docker

--- HTML/CSS/JavaScript/String Operations/sorting.js
+++ HTML/CSS/JavaScript/String Operations/sorting.js
@@ -1,62 +1,65 @@
-function bubble(){
-var swapp;
-   
-    var x=[12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
-     var n = x.length-1;
-    do {
-        swapp = false;
-        for (var i=0; i < n; i++)
-        {
-            if (x[i] > x[i+1])
-            {
-               var temp = x[i];
-               x[i] = x[i+1];
-               x[i+1] = temp;
-               swapp = true;
-            }
-        }
-        n--;
-    } while (swapp);
-
- alert(x);
-
-}
-function selection(){
-var A=[12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
-var len = A.length;
-    for (var i = 0; i < len - 1; i = i + 1) {
-        var j_min = i;
-        for (var j = i + 1; j < len; j = j + 1) {
-            if (A[j] < A[j_min]) {
-                j_min = j;
-            } else {}
-        }
-        if (j_min !== i) {
-            swap(A, i, j_min);
-        } else {}
-    }
-    alert(A);
-}
-
-function swap(A, x, y) {
-    var temp = A[x];
-    A[x] = A[y];
-    A[y] = temp;
-}
-function insertion(){
-var arr=[12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
-
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
-    let el = arr[i];
-    let j;
-
-    for (j = i - 1; j >= 0 && arr[j] > el; j--) {
-      arr[j + 1] = arr[j];
-    }
-    arr[j + 1] = el;
-  }
-  alert(arr);
-}
- 
-
+function selection()
+		{
+			var i,j,temp,num;
+			var ar=[];
+			num=prompt("Enter how many elements to be sorted:");
+			for(i=0;i<num;i++)
+			{
+				ar[i]=prompt('Enter element '+(i+1));
+			}
+        	for (i=0;i<num;i++){ 
+            	var min_idx = i; 
+            	for (j=i+1;j<num;j++){
+                	if (ar[j]<ar[min_idx])
+                	{ 
+                    	min_idx=j; 
+                    }
+            	}
+           		temp=ar[min_idx]; 
+            	ar[min_idx]=ar[i]; 
+            	ar[i]=temp; 
+            } 
+            alert(ar);
+		}
+		function insertion(){
+			var i,j,temp,num;
+			var ar=[];
+			num=prompt("Enter how many elements to be sorted:");
+			for(i=0;i<num;i++){
+				ar[i]=prompt('Enter element '+(i+1));
+			}
+			for(i=0;i<num;i++){
+    			j=i-1;
+    			temp=ar[i];
+    			while(j>=0 && ar[j]>temp)
+    			{
+      				ar[j+1]=ar[j];
+      				j--;
+    			}
+    			ar[j+1]=temp;
+  			}
+  			alert(ar);
+		}
+		function bubble()
+		{
+			var i,temp,num,swap;
+			var ar=[];
+			num=prompt("Enter how many elements to be sorted:");
+			for(i=0;i<num;i++){
+				ar[i]=prompt('Enter element '+(i+1));
+			}
+			
+			do{
+				swap=false;
+				for(i=0;i<num;i++){
+            		if(ar[i] > ar[i+1])
+            		{
+                		temp = ar[i];
+               			ar[i] = ar[i+1];
+               			ar[i+1] = temp;
+               			swap=true;
+            		}
+        		}
+        	}while(swap==true);
+        	alert(ar);
+		}
